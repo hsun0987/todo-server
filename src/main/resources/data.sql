9efa67914c7e1b69e0ca21ef8fb5901470690c92
hsun0987 <hsun0987@daum.net>
2025-07-09 13:44:27 +0900

    새로운 할 일 추가(등록) 기능 구현
    - Controller, Service, Repository 생성
    - Entity 및 데이터 객체 생성
    ** sql 파일 오류 / 빌드 멈춤 문제

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into TASK(`TITLE`, `DESCRIPTION`, `STATUS`, `DUE_DATE`)
+    values ('인텔리제이 강의 듣기', 'ch01 끝내기', 'IN_PROGRESS', now() + 1);
+
+insert into TASK(`TITLE`, `DESCRIPTION`, `STATUS`, `DUE_DATE`)
+    values ('인텔리제이 강의 듣기', 'ch02 끝내기', 'TODO', now() + 2);
