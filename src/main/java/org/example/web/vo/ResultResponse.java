0bd29f35536d63e3e52000170df7e9adaa538a83
hsun0987 <hsun0987@daum.net>
2025-07-09 15:03:32 +0900

    [#3] 할일 수정 및 삭제 기능 구현

---
+++ src/main/java/org/example/web/vo/ResultResponse.java
@@ -0,0 +1,13 @@
+package org.example.web.vo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ResultResponse {
+    private boolean isSuccess;
+}
